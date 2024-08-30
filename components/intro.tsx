654281b07597f118c7136eb5fb57dddd0b3191db
rupesh <rpiwal02@gmail.com>
2024-08-30 09:55:50 +0530

    intro updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,13 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <h1 className="text-[48px]">Rupesh Piwal</h1>
+        <h2>Fullstack Developer</h2>
+        <p className="text-[16px]">
+          Driven by a passion for building dynamic fullstack web applications, I
+          specialize in blending creativity with functionality to create
+          impactful and intuitive user experiences.
+        </p>
       </motion.h1>
 
       <motion.div
